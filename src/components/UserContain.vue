72057e32d570fa49748721592c092ccf0e62f149
1395204913 <1395204913@qq.com>
2019-08-25 16:09:13 +0800

    优化

--- src/components/UserContain.vue
+++ src/components/UserContain.vue
@@ -5,6 +5,75 @@
   
 </template>
 <script>
+</script>
+<style>
+ .user_page body {
+  background-color: #f6f6f6;
+  margin: 0px;
+  padding: 0px;
+}
+.user_page .header {
+  box-shadow: 4px 4px 4px #888888;
+  margin: 1rem;
+  background-color: #ffffff;
+}
+
+.user_page .userinfo {
+  padding-top: 10px;
+  display: flex;
+}
+
+.user_page .userinfo img {
+  border-radius: 50%;
+  border: 1px solid gray;
+}
+
+.user_page .userinfo p,
+.user_page .userinfo h3 {
+  display: inline;
+}
+
+.user_page .userinfo h3 {
+  margin-left: 10px;
+}
+
+.user_page .userinfo p {
+  margin-left: auto;
+  margin-right: 10px;
+  color: gray;
+  padding-top: 10px;
+}
+
+.user_page .chuangzuo {
+  display: flex;
+  justify-content: space-around;
+}
+
+.user_page .chuangzuo p {
+  display: inline;
+  font-weight: bold;
+}
+
+.user_page .chuangzuo span {
+  text-align: center;
+  padding: 5px 10px;
+}
+
+.user_page .gongneng_1,
+.user_page .gongneng_2 {
+  margin: 10px;
+  display: flex;
+  flex-wrap: wrap;
+  background-color: #ffffff;
+}
+
+.user_page .box img {
+  /* width: 4rem;
+            height: 3rem;
+            padding: 2rem; */
+  width: 45px;
+  margin: 15px;
+}
 
 
 </script>
